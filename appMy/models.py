30a9334c1ad8358643450d09d62b7adcb4be7f23
Berkay Ülger <berkayulger45920@gmail.com>
2023-06-22 12:09:59 +0300

    -

--- appMy/models.py
+++ appMy/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+   title = models.CharField(("Başlık"), max_length=50)
+   image = models.ImageField(("Kategori Resmi"), upload_to='category', max_length=200)
+   
+   def __str__(self):
+      return self.title
+
+
+
+class Card(models.Model):
+   category = models.ForeignKey(Category, verbose_name=("Kategori"), on_delete=models.CASCADE)
+   title = models.CharField(("Başlık"), max_length=50)
+   text = models.TextField(("İçerik"))
+   image = models.ImageField(("Kart Resmi"), upload_to='category', max_length=200)
+   price = models.FloatField(("Fiyat"))
+   isactive = models.BooleanField(("Aktif Kart"))
+   date_now = models.DateField(("Tarih"), auto_now_add=False)
+
+   def __str__(self):
+      return self.title
